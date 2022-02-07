070877353cb6d0fd506b0dc66887648ed5f38c5b
Ilya Fokin <ifokin@kr.digital>
2022-02-08 02:00:01 +0300

    fix: node start

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
-const { PORT, NODE_ENV } = process.env;
+const { NODE_ENV } = process.env;
+const PORT = process.env.PORT || 3000;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
