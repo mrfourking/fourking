461b0acbb126cc03ced8ef773e3ebbc85a58594c
Ilya Fokin <ifokin@kr.digital>
2022-02-10 00:31:00 +0300

    fix: return custom port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { NODE_ENV } = process.env;
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
