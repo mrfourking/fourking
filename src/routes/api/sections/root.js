7812c2a2b6079b2d94ac590a4f6ed1af6e202cb5
Ilya Fokin <ifokin@kr.digital>
2022-05-08 16:54:07 +0300

    chore: rm console.log

--- src/routes/api/sections/root.js
+++ src/routes/api/sections/root.js
@@ -3,12 +3,10 @@ import { getSections } from '../../../db'
 export async function get(req, res, next) {
     const sections = getSections().value()
 
-    console.log(sections)
-    
     if (sections !== null) {
       res.setHeader('Content-Type', 'application/json')
       res.end(JSON.stringify({sections}))
     } else {
-      // next()
+      next()
     }
   }
